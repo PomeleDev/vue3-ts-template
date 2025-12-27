fb36cb46596c93161143a5f1dd6dc3fa3399f5e7
𝙋𝙤𝙢𝙚𝙡𝙚𝘿𝙚𝙫 <poneledev@qq.com>
2025-12-28 05:14:16 +0800

    chore: 添加 ESLint 和 Prettier 配置，更新包依赖，并格式化代码风格

---
+++ eslint.config.ts
@@ -0,0 +1,30 @@
+import js from "@eslint/js"; // 校验js规范(推荐)
+import globals from "globals";
+import tseslint from "typescript-eslint"; // 推荐的ts规范
+import pluginVue from "eslint-plugin-vue"; // 推荐的vue规范
+import { defineConfig } from "eslint/config";
+import prettierRecommended from "eslint-plugin-prettier/recommended"; // 格式化
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.recommended,
+  pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"], // 校验vue中的ts规范
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  {
+    // 那些文件需要忽略校验
+    ignores: [".css", "*.d.ts"],
+  },
+  {
+    // 自定义规则
+    rules: {},
+  },
+  prettierRecommended, // 覆盖eslint的规范
+]);
---
+++ prettier.config.ts
@@ -0,0 +1,17 @@
+export default {
+  // 格式化
+  printWidth: 100, // 单行最大长度
+  tabWidth: 2, // 缩进宽度
+  useTabs: false,
+  // 引号
+  singleQuote: false, // 使用双引号
+  semi: true, // 使用分号
+  trailingComma: "es5",
+  // 括号
+  bracketSpacing: true, // 对象括号内加空格
+  arrowParens: "always",
+
+  vueIndentScriptAndStyle: false, // Vue文件script/style不额外缩进
+
+  endOfLine: "lf", // 换行符
+};
