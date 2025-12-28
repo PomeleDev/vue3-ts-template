37ee21b01fb714d714b9d4c43484df95b02aa073
𝙋𝙤𝙢𝙚𝙡𝙚𝘿𝙚𝙫 <zjn0629@126.com>
2025-12-28 09:28:58 +0800

    feat: 添加 Element Plus 组件库支持，更新样式和 App 组件结构

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,6 @@
   <RouterLink to="/about">关于</RouterLink>
   <RouterLink to="/">主页</RouterLink>
   <RouterView />
+
+  <el-button type="primary">测试</el-button>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import router from "./router";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+// 导入 element-plus 样式
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
 const app = createApp(App);
 const pinia = createPinia();
 
-app.use(router).use(pinia).mount("#app");
+app.use(router).use(pinia).use(ElementPlus).mount("#app");
